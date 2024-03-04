409990e07dab4c51c404ae49d65bc38af28dbc5b
wiktoworks <wiktor@wodzyn.pl>
2024-03-04 17:06:22 +0100

    tests

--- downmcload.sh
+++ downmcload.sh
@@ -1,2 +1,15 @@
-echo HEY!
-echo i still dont end dont use that
+#!/bin/bash
+
+items=(1 "install"
+       2 "uninstall")
+
+while choice=$(dialog --title "downmcload" \
+                 --menu "install / uninstall" 10 40 3 "${items[@]}" \
+                 2>&1 >/dev/tty)
+    do
+    case $choice in
+        1) echo install;; 
+        2) echo uninstall;; 
+        *) echo dick kurwa;; 
+    esac
+done
